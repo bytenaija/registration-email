d10528d643f59b9e9749ba2f1fd3f3aa2b08d6e1
Everistus Olumese <everistusolumese@gmail.com>
2018-12-23 11:03:18 +0100

    Added Tests

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # registration-email
 
+# To run this project
+You mut have node installed
+Have yarn/npm installed on your system
 
+
+
+## Available Scripts
+upstack
+In the project directory, you can run:
+
+### `yarn` or `npm install`
+Installs the dependencies
+
+### `yarn start` or `npm start`
+starts the API Server 
+
+### `yarn test` or `npm test`
+runs all tests
--- config/jwt.js
+++ config/jwt.js
@@ -1,7 +1,7 @@
 const jwt = require('jsonwebtoken');
 module.exports ={
   jwtSign : (user, cb) =>{
-    console.log("JWT", user)
+    // console.log("JWT", user)
     jwt.sign({user}, process.env.JWT_SECRET, (err, token)=>{
       if(err){
         cb(err)
@@ -15,7 +15,7 @@ module.exports ={
 
 verifyToken: (req, res, next)=> {
     const bearerHeader = req.headers['authorization'];
-    console.log(bearerHeader);
+    // console.log(bearerHeader);
     if (typeof bearerHeader != 'undefined') {
         const bearer = bearerHeader.split(" ");
         const bearerToken = bearer[1];
